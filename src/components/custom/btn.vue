a6463e13864cf5325d9df98606344eb06d32ab48
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-11 01:47:11 -0500

    Custom button

---
+++ src/components/custom/btn.vue
@@ -0,0 +1,50 @@
+<template>
+  <button class="btn" :style="styleBtn" @click="action">
+    {{ content.title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "btn",
+  components: {},
+  props: {
+    content: {
+      type: Object,
+      default: () => {},
+    },
+    styleBtn: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    action() {
+      this.$emit("actionBtn", this.content.action);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  outline: none;
+  cursor: pointer;
+  border: none;
+  background: transparent;
+  box-shadow: rgba(204, 219, 232) 8px 8px 20px,
+    rgba(255, 255, 255, 0.8) -8px -8px 20px;
+  border-radius: 10px;
+  color: var(--bg-secondary);
+  font-size: 25px;
+  transition: ease-in;
+}
+.btn:hover {
+  box-shadow: rgb(204, 219, 232) 3px 3px 6px 0px inset,
+    rgba(255, 255, 255, 0.8) -3px -3px 6px 1px inset;
+  transition: ease-in;
+}
+</style>
