8b0441bf5241681e6cbb323e77f4eba55f3106c0
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-11 01:16:19 -0500

    Component of level

---
+++ src/components/global/levels.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="levels">
+    <div class="containerLevel">
+      <button class="btnLevel btn" v-for="(level, i) of levels" :key="i">
+        {{ level.title }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "levels",
+  components: {},
+  data() {
+    return {
+      levels: [
+        { title: "Fácil" },
+        { title: "Intermedio" },
+        { title: "Difícil" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.levels {
+  width: 60vw;
+  min-height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--white);
+}
+.containerLevel {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+.btn {
+  outline: none;
+  cursor: pointer;
+  border: none;
+  background: transparent;
+}
+.btnLevel {
+  min-width: 200px;
+  height: 100px;
+  box-shadow: rgba(204, 219, 232) 8px 8px 20px,
+    rgba(255, 255, 255, 0.8) -8px -8px 20px;
+  border-radius: 10px;
+  color: var(--bg-secondary);
+  font-size: 25px;
+  transition: ease-in;
+}
+.btnLevel:hover {
+  box-shadow: rgb(204, 219, 232) 3px 3px 6px 0px inset,
+    rgba(255, 255, 255, 0.8) -3px -3px 6px 1px inset;
+  transition: ease-in;
+}
+@media (max-width: 480px) {
+  .levels {
+    width: 90vw;
+  }
+}
+</style>
---
+++ src/components/global/logo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="logo">
+    <img
+      src="https://site.innovasport.com/ux/iv-slide-marcas/-logo-adidas.png"
+      alt="logo de FabMemory"
+    />
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "logo",
+  components: {},
+  data() {
+    return {
+      title: "FabMemory",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  width: 60vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--white);
+}
+img {
+  width: auto;
+  height: 100px;
+}
+h1 {
+  font-size: 50px;
+}
+@media (max-width: 480px) {
+  .logo {
+    width: 90vw;
+  }
+  h1 {
+    font-size: 20px;
+  }
+}
+</style>
