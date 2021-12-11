81c48fe43eec18ab0d0d3b975d66892621da58ad
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-10 23:59:54 -0500

    Styles basic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    hola
-  </div>
+  <div id="app">hola</div>
 </template>
 
 <script>
@@ -12,12 +10,19 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+  background: red;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
