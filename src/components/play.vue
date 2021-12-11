745d96d5335bc52d7c7bd12f5f401dd3477c816a
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-11 04:30:27 -0500

    Created component Play

---
+++ src/components/play.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="play">
+    <header-play />
+    <div class="containerPlay">a</div>
+  </div>
+</template>
+
+<script>
+import headerPlay from "./global/headerPlay.vue";
+export default {
+  name: "play",
+  components: { headerPlay },
+  data() {
+    return {
+      title: "FabMemory",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.play {
+  display: grid;
+  grid-template-rows: 80px 1fr;
+  width: 650px;
+  height: 80vh;
+  gap: 20px;
+  box-sizing: border-box;
+}
+@media (max-width: 750px) {
+  .play {
+    width: 90vw;
+    height: 90vh;
+  }
+}
+</style>
