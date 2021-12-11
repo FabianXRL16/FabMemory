5869f941279cd3e4cd0b582f430d72a6ba5d8ec7
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-11 03:41:00 -0500

    Created modal of level

--- src/components/global/modalLevel.vue
+++ src/components/global/modalLevel.vue
@@ -1,17 +1,69 @@
 <template>
-  <div class="modalLevel">
-    <h1>hola</h1>
+  <div
+    class="modalLevel"
+    :class="
+      $store.state.modalLevel ? 'animationOpenModel' : 'animationOffModel'
+    "
+  >
+    <div class="title">
+      <h1>{{ currentLevel }}</h1>
+    </div>
+    <div class="containerLevels">
+      <btn
+        v-for="(level, i) of levels"
+        :class="level.state ? '' : 'disabled'"
+        :disabled="!level.state"
+        @actionBtn="selectLevel"
+        :key="i"
+        :content="level"
+        :styleBtn="styleLevels"
+      />
+    </div>
+    <div class="actions">
+      <btn
+        :content="{ title: 'Salir' }"
+        :styleBtn="style"
+        @actionBtn="closed"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import btn from "../custom/btn.vue";
 export default {
   name: "modalLevel",
-  components: {},
+  components: { btn },
   data() {
     return {
-      title: "Hola",
+      style: `width: 150px; height: 50px;`,
+      styleLevels: `width: 100%; height: 100%;`,
+      levels: [
+        { title: "Nivel 1", action: "1", state: true },
+        { title: "Nivel 2", action: "2", state: false },
+        { title: "Nivel 3", action: "3", state: false },
+        { title: "Nivel 4", action: "4", state: false },
+        { title: "Nivel 5", action: "5", state: false },
+      ],
     };
+  },
+  computed: {
+    currentLevel() {
+      let title = "";
+      let level = this.$store.state.level;
+      if (level === "f") title = "Fácil";
+      if (level === "i") title = "Intermedio";
+      if (level === "d") title = "Difícil";
+      return title;
+    },
+  },
+  methods: {
+    closed() {
+      this.$store.dispatch("showModalLevel");
+    },
+    selectLevel() {
+      console.log("hola");
+    },
   },
 };
 </script>
@@ -24,10 +76,74 @@ export default {
   top: 0;
   bottom: 0;
   margin: auto;
-  width: 200px;
-  height: 200px;
-  background: red;
+  width: 650px;
+  height: 400px;
+  background: var(--bg-primary);
+  border-radius: 10px;
+  box-shadow: rgba(204, 219, 232) 8px 8px 20px,
+    rgba(255, 255, 255, 0.8) -8px -8px 20px;
+  padding: 20px 40px;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-rows: 90px 1fr 70px;
+  color: var(--bg-secondary);
 }
-@media (max-width: 480px) {
+.animationOpenModel {
+  animation: showModal ease-in 0.3s;
+}
+@keyframes showModal {
+  to {
+    transform: scale(1);
+  }
+  from {
+    transform: scale(0);
+  }
+}
+.animationOffModel {
+  animation: hideModal 0.3s ease-in 2s;
+}
+@keyframes hideModal {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+.title {
+  display: grid;
+  place-content: center;
+  position: relative;
+}
+h1 {
+  margin: 0 0 10px 0;
+}
+.containerLevels {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-rows: repeat(3, 1fr);
+  gap: 10px;
+  place-content: center;
+}
+.actions {
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+}
+.disabled {
+  box-shadow: rgb(204, 219, 232) 3px 3px 6px 0px inset,
+    rgba(255, 255, 255, 0.8) -3px -3px 6px 1px inset;
+  transition: ease-in;
+  color: var(--bg-secondary-disabled);
+}
+@media (max-width: 750px) {
+  .modalLevel {
+    width: 90vw;
+    height: 90vh;
+    padding: 20px;
+  }
+  .containerLevels {
+    grid-template-rows: repeat(5, 1fr);
+  }
 }
 </style>
