c6ef9de7a7399eb18f5fc2db8670144a98ba86c2
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-11 02:03:14 -0500

    Component Modal level

---
+++ src/components/global/modalLevel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="modalLevel">
+    <h1>hola</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "modalLevel",
+  components: {},
+  data() {
+    return {
+      title: "Hola",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.modalLevel {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 200px;
+  height: 200px;
+  background: red;
+}
+@media (max-width: 480px) {
+}
+</style>
