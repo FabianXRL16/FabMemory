bbb8fb5b7f78ab809c613a2e456ccf9493149620
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-11 03:38:03 -0500

    Level in store

--- src/store.js
+++ src/store.js
@@ -6,18 +6,26 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     modalLevel: false,
+    level: "f",
   },
   getters: {
     getModalLevel: (state) => state.modalLevel,
+    getLevel: (state) => state.level,
   },
   actions: {
     showModalLevel({ commit }) {
       commit("CHANGE_STATE_MODAL");
     },
+    showLevel({ commit }, level) {
+      commit("SHOW_LEVEL", level);
+    },
   },
   mutations: {
     CHANGE_STATE_MODAL(state) {
       state.modalLevel = !state.modalLevel;
+    },
+    SHOW_LEVEL(state, level) {
+      state.level = level;
     },
   },
 });
