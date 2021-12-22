d5cb0020d49e8deb4af1260955367615fb89ce3b
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-22 02:16:42 -0500

    Refactoring code for star of level

--- src/components/custom/btn.vue
+++ src/components/custom/btn.vue
@@ -1,7 +1,24 @@
 <template>
-  <button class="btn" :style="styleBtn" @click="action">
+  <button
+    class="btn"
+    :class="content.completedCount >= 6 ? 'btnPro' : ''"
+    :style="styleBtn"
+    @click="action"
+  >
     {{ content.title }}
     <slot></slot>
+    <div
+      v-if="star"
+      class="completed"
+      :class="content.completedCount >= 4 ? 'pro' : ''"
+    >
+      <i
+        v-for="(n, i) in 3"
+        :key="n"
+        class="fas fa-star"
+        :style="`color: ${colorStar[i]}`"
+      ></i>
+    </div>
   </button>
 </template>
 
@@ -18,9 +35,25 @@ export default {
       type: String,
       default: "",
     },
+    star: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {};
+  },
+  computed: {
+    colorStar() {
+      let count = this.content.completedCount;
+      if (count === 1) return ["#34b7eb", "#acbcd1", "#acbcd1"];
+      if (count === 2) return ["#34b7eb", "#34b7eb", "#acbcd1"];
+      if (count === 3) return ["#34b7eb", "#34b7eb", "#34b7eb"];
+      if (count === 4) return ["#ebbd34", "#34b7eb", "#34b7eb"];
+      if (count === 5) return ["#ebbd34", "#ebbd34", "#34b7eb"];
+      if (count >= 6) return ["#ebbd34", "#ebbd34", "#ebbd34"];
+      else return ["#acbcd1", "#acbcd1", "#acbcd1"];
+    },
   },
   methods: {
     action() {
@@ -48,10 +81,31 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
+}
+.btnPro {
+  color: #ebbd34;
+ font-size: 28px;
 }
 .btn:hover {
   box-shadow: rgb(204, 219, 232) 3px 3px 6px 0px inset,
     rgba(255, 255, 255, 0.8) -3px -3px 6px 1px inset;
   transition: ease-in;
+}
+.completed {
+  display: flex;
+  align-items: flex-end;
+}
+i {
+  margin-top: 3px;
+  font-size: 10px;
+}
+i:nth-child(2) {
+  margin-top: -1px;
+  font-size: 13px;
+}
+.pro {
+  transform: scale(1.2);
+  margin-top: 3px;
 }
 </style>
